bbc32d8b07ad16e49f9b1235a4125acf26e00aaf
Kevin Ayala <ayala.kevin99@gmail.com>
2021-01-18 19:28:33 -0800

    set up main, josh to do hw

--- main.py
+++ main.py
@@ -1,2 +1,47 @@
-#this file's objective is to send data to a Cloudflare Worker in order to host our game
+# this file's objective is to send data to a Cloudflare Worker in order to host our game
 
+# lets first install the package we need: requests
+import requests
+
+# constants: URL of our website
+URL = "https://josh-game.kayala.workers.dev/"
+
+# make a function to send information to our website
+
+
+def sendMessage(type: str, user: str, message: str):
+    if (type == "GET"):
+        response = requests.get(URL)
+        print(response.text)
+    else:
+        # POST
+
+        # first, let's make the JSON object
+        json = "{user:" + user + ",message:" + message + "}"
+
+        response = requests.post(URL, data=json)
+
+
+def main():
+    print("This is josh's hw!")
+
+    # take information using input()
+    typeofMsg = ""
+    user = ""
+    msg = ""
+
+    # 1. ask the user whether they're doing a POST or a GET and write down the response as the variable "type"
+
+    # 2. ask the user what their name is and write it down as the variable "user"
+
+    # 3. ask the user what their message is and write it down as the variable "message"
+
+    sendMessage(typeofMsg, user, msg)
+
+    sendMessage("POST", "kevin", "hi!")
+    sendMessage("GET", "kevin", "none")
+
+
+# don't touch this :)
+if __name__ == "__main__":
+    main()
