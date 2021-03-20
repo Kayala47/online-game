3dba9f735cf1f3170624fb125ae2fe3ca68f9b04
Kevin Ayala <ayala.kevin99@gmail.com>
2021-03-19 19:05:14 -0700

    showed josh how to set defaults

--- game/wizard.py
+++ game/wizard.py
@@ -1,7 +1,7 @@
 import pygame
 
 
-wizard = pygame.image.load("..\\sprites\\wizard.png")
+# wizard = pygame.image.load("..\\sprites\\wizard.png")
 START_X = 100
 END_x = 470
 GROUND = 200
@@ -15,16 +15,18 @@ class Wizard(pygame.sprite.Sprite):
 
     '''
 
-    def __init__(self, place, sound_set):
+    def __init__(self, place: str, sound_set: float = 0.1):
         '''
+        Description:
         Init is initalizing  these varibles to set things that the client.py is using to use in fuctions below
         Input:
         place is the part where the wizard might show up. Possible inputs are "left" or "right"
-        sound_set is used to tell what volume level the user wants. the sound levels can be set to 0.0 - 1
+        sound_set is used to tell what volume level the user wants. the sound levels can be set to 0.0 - 1. Defaults to 0.1, which Josh says is already too loud
         '''
 
         # super().__init__()
 
+        # this does blah blah
         wizard_blast.set_volume(sound_set)
 
         if (place == "left"):
@@ -36,3 +38,23 @@ class Wizard(pygame.sprite.Sprite):
         pygame.mixer.Sound.play(wizard_blast)
 
         pygame.display.update()
+
+
+def test():
+    # this is supposed to be a comment, but is not an unconnected string
+    "hello"
+    # '''
+    # This function exitss entirely to mess with Josh
+    # Inputs:
+    # none
+    # '''
+    print("hi!")
+
+
+print(test.__doc__)
+
+'''
+This is actually a free floating string. But the computer doesn't do anything with it.
+
+
+'''
