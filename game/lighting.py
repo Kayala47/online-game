706d816b5c4ef2e9e10224cc50f075ff2130aae4
unknown <lulukim78709@gmail.com>
2021-06-04 18:15:11 -0700

    hm

---
+++ game/lighting.py
@@ -0,0 +1,15 @@
+import pygame
+from wizard import Wizard
+
+
+
+
+
+class Lighting(pygame.sprite.Sprite):
+
+    self.wiz = wiz
+    def __init__ (self, wiz):
+        
+        
+        self.image = pygame.image.load("..\\sprites\\lighting.png")
+
