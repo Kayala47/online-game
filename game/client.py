c52a72260cd65d2c99a1347511392fe8baf89cd6
unknown <lulukim78709@gmail.com>
2021-06-18 15:05:10 -0700

    homework

--- game/client.py
+++ game/client.py
@@ -23,6 +23,9 @@ bg = pygame.image.load("..\\sprites\\better-background.png")
 fire_sign = pygame.image.load("..\\sprites\\fireball-sign.png")
 fire_signHB = fire_sign.get_rect(topleft=(800, 100))
 
+lighting_sign = pygame.image.load("..\\sprites\\lighting-sign.png")
+lighting_signHB = lighting_sign.get_rect(topleft=(1000, 100))
+
 
 
 # wizard = pygame.image.load("..\\sprites\\wizard.png")
@@ -50,6 +53,7 @@ while True:
 
     win.blit(bg, (0,0))
     win.blit(fire_sign, (800, 100))
+    win.blit(lighting_sign, (1000, 100))
     wizard.update()
     pygame.display.update()
 
@@ -60,6 +64,7 @@ while True:
 
         win.blit(bg, (0,0))
         win.blit(fire_sign, (800, 100))
+        win.blit(lighting_sign, (1000, 100))
         wizard.update()
         pygame.display.update()
 
@@ -72,6 +77,7 @@ while True:
             repeat = fb.move()
             win.blit(bg, (0,0))
             win.blit(fire_sign, (800, 100))
+            win.blit(lighting_sign, (1000, 100))
             wizard.update()
             pygame.display.update()
         
@@ -88,6 +94,7 @@ while True:
                     
                     print(str(mousePos)+"this one")
                     print(str(fire_signHB)+"this one")
+                    print(str(lighting_signHB)+"maybe this one")
                     
 
                     if pygame.Rect.collidepoint(fire_signHB, mousePos):
@@ -98,13 +105,23 @@ while True:
                             win.blit(fb.get_image(), fb.get_pos())
                         else:
                             repeat = False
+                            
 
+                    if pygame.Rect.collidepoint(lighting_signHB, mousePos):
+                        
+                        fb = Fireball(win, 10, wizard, wizard)
+                        if fb.move() == True:
+                            repeat = True
+                            win.blit(fb.get_image(), fb.get_pos())
+                        else:
+                            repeat = False
                         
                     # if pygame.Rect.collidepoint(wizard.hitbox, mousePos):
                     #     if (wizard.take_damage(10) == True):
                             
         win.blit(bg, (0,0))
         win.blit(fire_sign, (800, 100))
+        win.blit(lighting_sign, (1000, 100))
         wizard.update()
         pygame.display.update()
 
@@ -115,6 +132,7 @@ while True:
     else:
         win.blit(bg, (0,0))
         win.blit(fire_sign, (800, 100))
+        win.blit(lighting_sign, (1000, 100))
         wizard.update()
         pygame.display.update()
         
@@ -125,6 +143,7 @@ while True:
 
     win.blit(bg, (0,0))
     win.blit(fire_sign, (800, 100))
+    win.blit(lighting_sign, (1000, 100))
     wizard.update()
     pygame.display.update()
 # while True:
